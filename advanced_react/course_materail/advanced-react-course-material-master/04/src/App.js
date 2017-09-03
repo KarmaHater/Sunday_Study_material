c17032ede28a257d9d939ff184ed3f51b364a03b
KarmaHater <andra.lally@gmail.com>
2017-09-03 15:39:13 +0200

    update side order comp

--- advanced_react/course_materail/advanced-react-course-material-master/04/src/App.js
+++ advanced_react/course_materail/advanced-react-course-material-master/04/src/App.js
@@ -1,54 +1,59 @@
-
+import "./index.css";
+import React from "react";
+import MenuIcon from "react-icons/lib/md/menu";
+import { set, get, subscribe } from "./local-storage";
 /*
 Create a `withStorage` higher order component that manages saving and retrieving
 the `sidebarIsOpen` state to local storage
 */
 
-import './index.css'
-import React from 'react'
-import MenuIcon from 'react-icons/lib/md/menu'
-import { set, get, subscribe } from './local-storage'
+const withStorage = (storageKey, default_) => (Comp) =>
+    class extends React.Component {
+        state = {
+            [storageKey]: get(storageKey, default_)
+        };
+
+        componentDidMount() {
+            this.unsubscribe = subscribe(() => {
+                this.setState({
+                    sidebarIsOpen: get("sidebarIsOpen")
+                });
+            });
+        }
+
+        componentWillUnmount() {
+            this.unsubscribe();
+        }
+
+        render() {
+            return <Comp {...this.state} />;
+        }
+    };
 
 class App extends React.Component {
-  state = {
-    sidebarIsOpen: get('sidebarIsOpen', true)
-  }
+    render() {
+        const { sidebarIsOpen } = this.props;
+        return (
+            <div className="app">
+                <header>
+                    <button
+                        className="sidebar-toggle"
+                        title="Toggle menu"
+                        onClick={() => {
+                            set("sidebarIsOpen", !sidebarIsOpen);
+                        }}
+                    >
+                        <MenuIcon />
+                    </button>
+                </header>
 
-  componentDidMount() {
-    this.unsubscribe = subscribe(() => {
-      this.setState({
-        sidebarIsOpen: get('sidebarIsOpen')
-      })
-    })
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
-  render() {
-    const { sidebarIsOpen } = this.state
-    return (
-      <div className="app">
-        <header>
-          <button
-            className="sidebar-toggle"
-            title="Toggle menu"
-            onClick={() => {
-              set('sidebarIsOpen', !sidebarIsOpen)
-            }}
-          >
-            <MenuIcon/>
-          </button>
-        </header>
-
-        <div className="container">
-          <aside className={sidebarIsOpen ? 'open' : 'closed'}/>
-          <main/>
-        </div>
-      </div>
-    )
-  }
+                <div className="container">
+                    <aside className={sidebarIsOpen ? "open" : "closed"} />
+                    <main />
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App
+export default withStorage('sidebarIsOpen', true)(App);
